b1bd7dd3aba3bb514542a256f58fda2f62c416c4
Bob Starr <bobstarr@rtzaudio.com>
2026-07-03 19:20:55 -0400

    Adding connect thread

--- TCPConnectThread.cpp
+++ TCPConnectThread.cpp
@@ -14,6 +14,43 @@ TCPConnectThread::~TCPConnectThread()
     delete m_pSocket;
 }
 
+wxThread::ExitCode TCPConnectThread::Entry()
+{
+    // Create the socket and perform connection here
+    wxIPV4address addr;
+    addr.Hostname(m_host);
+    addr.Service(m_port);
+
+    m_pSocket = new wxSocketClient();
+
+    // Connect asynchronously or use a timeout so you can check TestDestroy()
+    m_pSocket->Connect(addr, false); // false = don't block main loop
+
+    // Wait for connection or cancellation
+    while (!TestDestroy() && !m_pSocket->WaitOnConnect(100))
+    {
+        // Wait loop
+    }
+
+    if (TestDestroy())
+    {
+        if (m_pSocket)
+        {
+            m_pSocket->Destroy();
+            m_pSocket = nullptr;
+        }
+
+        return (ExitCode)0;
+    }
+
+    // Notify parent on completion
+    wxThreadEvent event(wxEVT_THREAD, wxID_OK);
+    wxQueueEvent(m_pParent, event.Clone());
+
+    return (ExitCode)0;
+}
+
+
 #if 0
 wxThread::ExitCode TCPConnectThread::Entry()
 {
@@ -38,37 +75,3 @@ wxThread::ExitCode TCPConnectThread::Entry()
 }
 #endif
 
-wxThread::ExitCode TCPConnectThread::Entry()
-{
-    // Create the socket and perform connection here
-    wxIPV4address addr;
-    addr.Hostname(m_host);
-    addr.Service(m_port);
-
-    m_pSocket = new wxSocketClient();
-
-    // Connect asynchronously or use a timeout so you can check TestDestroy()
-    m_pSocket->Connect(addr, false); // false = don't block main loop
-
-    // Wait for connection or cancellation
-    while (!TestDestroy() && !m_pSocket->WaitOnConnect(100)) {
-        // Wait loop
-    }
-
-    if (TestDestroy())
-    {
-        if (m_pSocket)
-        {
-            m_pSocket->Destroy();
-            m_pSocket = nullptr;
-        }
-
-        return (ExitCode)0;
-    }
-
-    // Notify parent on completion
-    wxThreadEvent event(wxEVT_THREAD, wxID_OK);
-    wxQueueEvent(m_pParent, event.Clone());
-
-    return (ExitCode)0;
-}
