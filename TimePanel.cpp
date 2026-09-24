5cf9741968825471bc65fecf56ce21d5b993c735
Bob Starr <rtzaudio@comcast.net>
2026-09-24 17:10:26 -0400

    Update TimePanel.cpp

--- TimePanel.cpp
+++ TimePanel.cpp
@@ -185,7 +185,7 @@ void TimePanel::Draw(wxDC& dc)
 		else
 			strMode = _T("OFF");
 
-        str.Printf(wxT("SMPTE ") + strMode);
+        str = wxT("SMPTE ") + strMode;
         sizeText = dc.GetTextExtent(str);
         dc.DrawText(str, xpos, ypos);
         xpos += sizeText.GetWidth() + xSpace;
